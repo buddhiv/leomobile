3ea6e3c4a2d724ff6fb72aa5088dd9ef101b5e6c
sujee saparamadu <sujeesaparamadu@sujees-MacBook-Pro.local>
2020-03-23 11:50:46 +0530

    adding changes to webservice, initial webservice call in clubs screen, implementation of the no network connection message, adding init service

--- app/core/Layout.js
+++ app/core/Layout.js
@@ -3,23 +3,50 @@ import {
     SafeAreaView,
     StyleSheet,
     StatusBar,
-    ScrollView
+    ScrollView,
+    Text,
+    View
 } from 'react-native';
+import InitService from '../lib/services/InitService';
+import GlobalService from '../lib/services/GlobalService';
 
-const Layout: () => React$Node = (props) => {
-    return (
-        <>
-            <StatusBar barStyle="light-content"/>
-            <SafeAreaView>
-                <ScrollView
-                    contentInsetAdjustmentBehavior="automatic">
+class Layout extends React.Component {
+    constructor(props) {
+        super(props);
 
-                    {props.children}
+        this.state = {
+            is_connected: GlobalService.get('connected_to_internet')
+        }
+    }
 
-                </ScrollView>
-            </SafeAreaView>
-        </>
-    );
+    componentDidMount(): void {
+        InitService.getGlobalEventEmitter().addListener('connection_change', (payload) => {
+            if(payload.connection_changed !== this.state.is_connected){
+                this.setState({is_connected: payload.connection_changed});
+            }
+        });
+    }
+
+    render(): React.ReactElement<any> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
+        return (
+            <>
+                <StatusBar barStyle="light-content"/>
+
+                {!this.state.is_connected ? <View style={{backgroundColor: 'red', paddingHorizontal: 15}}>
+                    <Text style={{color: 'white'}}>No Network Connection</Text>
+                </View>: null}
+
+                <SafeAreaView>
+                    <ScrollView
+                        contentInsetAdjustmentBehavior="automatic">
+
+                        {this.props.children}
+
+                    </ScrollView>
+                </SafeAreaView>
+            </>
+        )
+    }
 };
 
 const styles = StyleSheet.create({});
