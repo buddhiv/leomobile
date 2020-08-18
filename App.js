c622188ee5384c8f3126b679b59e860874f031cb
Yasiru NIlan <yasiru.13@cse.mrt.ac.lk>
2020-08-18 09:47:25 +0530

    added sentry to project

--- App.js
+++ App.js
@@ -12,6 +12,13 @@ import MainScreen from './app/screens/core/MainScreen';
 import {Provider} from 'react-redux';
 import configureStore from './app/redux/store/ConfigureStore';
 
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({ 
+  dsn: 'https://4b644d40738f496c84fe3a65b80e38dd@o435475.ingest.sentry.io/5394846', 
+});
+
+
 const store = configureStore();
 
 const App: () => React$Node = () => {
