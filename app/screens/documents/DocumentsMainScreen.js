8a716e3a5aa284deaa63cf69066a75a07ac8b33c
buddhiv <92buddhiv@gmail.com>
2020-08-19 09:46:52 +0530

    file rename

---
+++ app/screens/documents/DocumentsMainScreen.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import DocumentsScreen from './DocumentsScreen';
+import SingleDocumentsScreen from './SingleDocumentScreen';
+
+import ColorService from '../../common/services/ColorService';
+
+const DocumentsMainScreen: () => React$Node = (props) => {
+    const Stack = createStackNavigator();
+
+    return (
+        <>
+            <Stack.Navigator initialRouteName={'Documents'}
+                             screenOptions={{
+                                 headerTintColor: 'white',
+                                 headerStyle: {
+                                     backgroundColor: ColorService.PRIMARY_COLOR,
+                                 },
+                             }}>
+                <Stack.Screen name="Documents" component={DocumentsScreen}/>
+                <Stack.Screen name="Document" component={SingleDocumentsScreen}
+                              options={({route}) => ({title: route.params.name})}/>
+            </Stack.Navigator>
+        </>
+    );
+};
+
+export default DocumentsMainScreen;
